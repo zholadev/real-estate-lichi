91e09fff7145fae804786f268ca8ae3aaa76e29b
zholadev <zholadev.io@gmail.com>
2023-10-20 11:51:36 +0600

    animate tab is added

---
+++ src/shared/uikit/motion/ui/MotionTextUnderLine.jsx
@@ -0,0 +1,35 @@
+'use client'
+
+import React from 'react';
+import {motion} from 'framer-motion'
+import styles from '@/styles/ui-tabs.module.sass'
+
+/**
+ * @author Zholaman Zhumanov
+ * @created 20.10.2023
+ * @param props
+ * @returns {Element}
+ * @constructor
+ */
+function MotionTextUnderLine(props) {
+    const {current, id, style} = props
+
+    return (
+        current === id &&
+        <motion.div
+            layoutId={'borderBottom'}
+            style={{
+                width: "100%",
+                height: "2px",
+                position: "absolute",
+                bottom: "-4.1px",
+                left: 0,
+                display: "block",
+                backgroundColor: "rgba(22, 24, 29, 1)",
+                ...style
+            }}
+        />
+    );
+}
+
+export default MotionTextUnderLine;
