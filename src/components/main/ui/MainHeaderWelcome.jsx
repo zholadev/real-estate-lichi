d584534fc86a1a8d850a891fc0ae2d3eab16fa4e
zholadev <zholadev.io@gmail.com>
2024-01-16 17:26:43 +0600

    main video -v sm poster updated

--- src/components/main/ui/MainHeaderWelcome.jsx
+++ src/components/main/ui/MainHeaderWelcome.jsx
@@ -33,7 +33,7 @@ function MainHeaderWelcome(props) {
                         src={'https://player.vimeo.com/progressive_redirect/playback/877839534/rendition/720p/file.mp4?loc=external&signature=789b1251249677aa4c66d5a8f5b83ebe86ec85ad25af29fdb87a696fdd20f74a'}
                         isBorderRadius
                         isMobile={mediaQuerySm}
-                        poster={IMG.posterDubaiMain['src']}
+                        poster={mediaQuerySm ? IMG.posterDubaiMainSm['src'] : IMG.posterDubaiMain['src']}
                         posterStyle={{borderRadius: '4px'}}
                     />
                 </figure>
